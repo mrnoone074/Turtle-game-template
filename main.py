cbfd05ba7b10c4782c3fca7b547dac299be9077d
mrnoone074 <96616764+mrnoone074@users.noreply.github.com>
2022-04-22 13:09:35 +0000

    i’ve added comments so its easier to understand.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# imports
 import turtle
 
+# window setup
+# win = window
 win = turtle.Screen()
 win.setup(width=600, height=400)
 win.tracer()
 
+# game loop
 closed = False
 while not closed:
     win.update()
